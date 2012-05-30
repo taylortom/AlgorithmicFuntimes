b1983e02873c812b5b09615e0d4504dc6cdc4927
Thomas Taylor <tom@taylortom.co.uk>
2012-05-30 16:25:49 +0100

    Implemented in-place quicksort

--- QuicksortInPlace.py
+++ QuicksortInPlace.py
@@ -1,6 +1,13 @@
 #
-# This version reduces the required storage space from 
-# O(n) to O(log n)
+# In-place Quicksort
+# Time: O(n log n) O(n2) worst-case 
+# Space: O(log n)
+#
+# Partition algorithm moves all elements less than a pivot point to one part of the array, 
+# and all elements larger to the other part, returning the final position of the pivot in the array.
+#
+# Each call to the algorithm reduces the array by at least one element, since the element at pivotNewIndex 
+# is placed in its final position.
 #
 
 import random
@@ -14,34 +21,46 @@ sorted = []
 
 # create a list of random numbers bounded by max
 while len(listToSort) < max:
-	rand = round(random.random() * 100)
+	rand = round(random.random() * max)
 	while rand > max:
-		rand = round(random.random() * 100)
+		rand = round(random.random() * max)
 	listToSort.append(rand)
 
+# swaps the two elements in the passed list
 def swap(list, index1, index2):
-	index1Value = list[index1]
-	list[index1] = list[index2]
-	list[index2] = index1Value
-
+	index1Val = list[index1]
+	index2Val = list[index2]
+	list[index1] = index2Val
+	list[index2] = index1Val
+	
+# sorts the 
 def partition(list, left, right, pivotIndex):
-	pivot = list[pivotIndex]
+	pivotVal = list[pivotIndex]
 	swap(list, pivotIndex, right)
 	storeIndex = left
 	
-	for i in range(len(list)):
-		if list[i] < pivot:
-			swap(list, i, storeIndex)
-			storeIndex = storeIndex+1
+	for index in range(left, right):
+		if list[index] < pivotVal: 
+			swap(list, index, storeIndex)
+			storeIndex = storeIndex + 1
+			
 	swap(list, storeIndex, right)
 	return storeIndex
-
-def quicksort(toSort, left, right):
+			
+# sort the list
+def quicksort(list, left, right):
 	if left < right:
-		pivotIndex = (left+right)/2
-		pivotNewIndex = partition(toSort, left, right, pivotIndex)
-		quicksort(toSort, left, pivotNewIndex-1)
-		quicksort(toSort, pivotNewIndex+1, right)
+		
+		# get a pivot index
+		pivotIndex = int(round(random.random() * max))
+		while pivotIndex < left or pivotIndex > right: 
+			pivotIndex = int(round(random.random() * max))
+		
+		pivotNewIndex = partition(list, left, right, pivotIndex)
 
-sorted = quicksort(listToSort, 0, max-1)
-print 'sorted list:', sorted
+		# Recursively sort elements on each side
+		quicksort(list, left, pivotNewIndex - 1)
+		quicksort(list, pivotNewIndex + 1, right)
+		
+quicksort(listToSort, 0, len(listToSort) - 1)
+print "SORTED GUV:", listToSort
